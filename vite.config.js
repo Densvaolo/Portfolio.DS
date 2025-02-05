55cdf848f65134e3c7906c7034fd74e171bb3f65
Dennis Svarts <133116854+Densvaolo@users.noreply.github.com>
2025-02-05 17:04:55 +0100

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: './', // För att undvika sökvägsproblem
-    build: {
-        outDir: 'dist', // Se till att utdata hamnar i dist/
-    },
-});
+    plugins: [react()],
+    base: '/my-vite-app/'
+})
