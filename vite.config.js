31448f837d320fc3da169686e88ec00068b745da
Dennis Svarts <133116854+Densvaolo@users.noreply.github.com>
2025-02-06 19:28:53 +0100

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Portfolio.DS/', // Se till att basvägen matchar din GitHub Pages-webbplatsens URL
+    base: '/Portfolio.DS/', // Lägg till din GitHub Pages-sökväg här
 });
