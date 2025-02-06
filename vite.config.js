9ed9a88b7120737205803e4faf11b088e788bfba
Dennis Svarts <133116854+Densvaolo@users.noreply.github.com>
2025-02-06 20:13:16 +0100

    gg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    root: '.', // Förväntar att alla filer är i projektets rot
-    build: {
-        outDir: 'dist', // Byggresultatet sparas i dist/
-    },
-})
+    plugins: [react()],
+    base: '/Portfolio.DS/', // Lägg till din GitHub Pages-sökväg här
+});
